bb9c0046c6893ff032e532d1bfdbb988ea458e9b
paulpassauer <58029670+paulpassauer@users.noreply.github.com>
2020-03-12 10:42:05 +0100

    funktion 3 erweitert

--- test.php
+++ test.php
@@ -2,7 +2,6 @@
   echo "Das ist unser erstes Update";
   echo "Das ist unser zweites Update";
 
-<<<<<<< HEAD
   function funktion2() {
 
  string a='Paul programmier mal !!';
@@ -11,10 +10,12 @@
  }
 
 
-=======
   function funktion1()
   {
     return null;
   }
->>>>>>> f9d57c7661f98a1d98bca68a3c529ba05e7b037c
+  function funktion3()
+  {
+    return null;
+  }
  ?>
