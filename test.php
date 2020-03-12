348c3ebc73a761b93ec25e2196e30654a80b201e
lennygit <58029660+lennygit@users.noreply.github.com>
2020-03-12 10:38:12 +0100

    Funktion 2

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
   echo "Das ist unser erstes Update";
   echo "Das ist unser zweites Update";
+
+  function funktion2() {
+
+ string a='Paul programmier mal !!';
+
+   return a;
+ }
+
+
  ?>
